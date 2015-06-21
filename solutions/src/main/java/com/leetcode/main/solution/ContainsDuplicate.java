263e730781dd0cb4d20ae95f7cdc82d8901a60a2
spandand <spandand@cloudera.com>
2015-06-21 14:34:48 -0700

    Code Reformatted.

--- solutions/src/main/java/com/leetcode/main/solution/ContainsDuplicate.java
+++ solutions/src/main/java/com/leetcode/main/solution/ContainsDuplicate.java
@@ -2,13 +2,13 @@ package com.leetcode.main.solution; /**
  * https://leetcode.com/problems/contains-duplicate/
  * Status = Accepted
  */
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
   public boolean containsDuplicate(int[] nums) {
     Set<Integer> seen = new HashSet<Integer>();
-
     for (int x : nums) {
       if (seen.contains(x)) {
         return true;
