f7eb2127d89919c6a1de12f13748480e2352c440
spandand <spandand@cloudera.com>
2015-06-20 23:41:10 -0700

    Solve Contains-duplicate
    https://leetcode.com/problems/contains-duplicate/

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/contains-duplicate/
+ * Status = Accepted
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> seen = new HashSet<Integer>();
+
+    for (int x : nums) {
+      if (seen.contains(x)) {
+        return true;
+      }
+      seen.add(x);
+    }
+    return false;
+  }
+}
---
+++ src/test/java/TestContainsDuplicate.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestContainsDuplicate {
+  private static ContainsDuplicate soln;
+
+  @BeforeClass
+  public static void setUp() {
+    soln = new ContainsDuplicate();
+  }
+
+  @Test
+  public void testContainsDuplicate() {
+    Assert.assertFalse(soln.containsDuplicate(new int[]{}));
+    Assert.assertFalse(soln.containsDuplicate(new int[]{1, 2}));
+    Assert.assertTrue(soln.containsDuplicate(new int[]{5, 5, 5}));
+    Assert.assertFalse(soln.containsDuplicate(new int[]{1, 2, 3, 4}));
+    Assert.assertTrue(soln.containsDuplicate(new int[]{4, 5, 6, 7, 6}));
+  }
+}
