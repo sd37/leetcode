7d91c425e3f7f44f7ca169d3be0fb4abf3b0c966
spandand <spandand@cloudera.com>
2015-06-21 20:42:19 -0700

    Solve Binary Tree Level Order Traversal
    https://leetcode.com/problems/binary-tree-level-order-traversal/

---
+++ solutions/src/main/java/com/leetcode/main/solution/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,55 @@
+/**
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Status = Accepted
+ */
+package com.leetcode.main.solution;
+
+import com.leetcode.common.TreeNode;
+
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversal {
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> levelOrder = new ArrayList<List<Integer>>();
+
+    if (root == null) {
+      return levelOrder;
+    }
+
+    Map<Integer, List<Integer>> levelNodes = new TreeMap<Integer, List<Integer>>();
+    Map<TreeNode, Integer> nodeLevel = new HashMap<TreeNode, Integer>();
+
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
+    q.add(root);
+    nodeLevel.put(root,0);
+    levelNodes.put(nodeLevel.get(root), new ArrayList<Integer>());
+    levelNodes.get(nodeLevel.get(root)).add(root.val);
+
+    while (!q.isEmpty()) {
+      TreeNode curr = q.poll();
+      int nextLevel = nodeLevel.get(curr) + 1;
+      if (curr.left != null) {
+        q.add(curr.left);
+        nodeLevel.put(curr.left, nextLevel);
+        if (!levelNodes.containsKey(nextLevel)) {
+          levelNodes.put(nextLevel, new ArrayList<Integer>());
+        }
+        levelNodes.get(nextLevel).add(curr.left.val);
+      }
+
+      if (curr.right != null) {
+        q.add(curr.right);
+        nodeLevel.put(curr.right, nextLevel);
+        if (!levelNodes.containsKey(nextLevel)) {
+          levelNodes.put(nextLevel, new ArrayList<Integer>());
+        }
+        levelNodes.get(nextLevel).add(curr.right.val);
+      }
+    }
+
+    for (Integer i : levelNodes.keySet()) {
+      levelOrder.add(levelNodes.get(i));
+    }
+    return levelOrder;
+  }
+}
---
+++ solutions/src/test/java/com/leetcode/test/solution/TestBinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,55 @@
+package com.leetcode.test.solution;
+
+import com.leetcode.common.TreeNode;
+import com.leetcode.main.solution.BinaryTreeLevelOrderTraversal;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestBinaryTreeLevelOrderTraversal {
+  private static BinaryTreeLevelOrderTraversal soln;
+
+  @BeforeClass
+  public static void setUp() {
+    soln = new BinaryTreeLevelOrderTraversal();
+  }
+
+  @Test
+  public void testLevelOrder() {
+    /**
+     *     3
+     *    /  \
+     *   9   20
+     *  /  \
+     * 5    7
+     */
+
+    TreeNode root = new TreeNode(3);
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(20);
+    root.left.left = new TreeNode(15);
+    root.left.right = new TreeNode(7);
+    root.right = new TreeNode(20);
+
+    List<List<Integer>> levelOrder = soln.levelOrder(root);
+
+    List<Integer> l1 = new ArrayList<Integer>();
+    l1.add(3);
+
+    List<Integer> l2 = new ArrayList<Integer>();
+    l2.add(9);
+    l2.add(20);
+
+    List<Integer> l3 = new ArrayList<Integer>();
+    l3.add(15);
+    l3.add(7);
+
+    Assert.assertEquals(3, levelOrder.size());
+    Assert.assertEquals(l1, levelOrder.get(0));
+    Assert.assertEquals(l2, levelOrder.get(1));
+    Assert.assertEquals(l3, levelOrder.get(2));
+  }
+}
