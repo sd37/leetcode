ca13cd7b14b2538aef344db4ef54e413b1d74606
spandand <spandand@cloudera.com>
2015-12-22 17:10:19 -0800

    MissingNumber added

---
+++ solutions/src/main/java/com/leetcode/main/solution/MissingNumber.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/missing-number/
+package com.leetcode.main.solution;
+public class MissingNumber {
+  public int missingNumber(int[] nums) {
+    if(nums == null) {
+      throw new RuntimeException("nums == null");
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i <= nums.length; i++) {
+      ans ^= i;
+    }
+
+    for (int x : nums) {
+      ans ^= x;
+    }
+
+    return ans;
+  }
+}
