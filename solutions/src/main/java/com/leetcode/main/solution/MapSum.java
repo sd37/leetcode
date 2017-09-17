28037766639af9dd27e0466b01482bc83d73906e
spandan <spandan@prosperworks.com>
2017-09-17 12:49:30 -0700

    MapSum added

--- solutions/src/main/java/com/leetcode/main/solution/MapSum.java
+++ solutions/src/main/java/com/leetcode/main/solution/MapSum.java
@@ -1,21 +1,31 @@
+// https://leetcode.com/contest/leetcode-weekly-contest-50/problems/map-sum-pairs/
+// status = AC
+
 package com.leetcode.main.solution;
 
-public class MapSum {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MapSum {
 
   /**
    * Initialize your data structure here.
    */
-  public MapSum() {
 
+  private Map<String, Integer> mapSum;
+
+  public MapSum() {
+    mapSum = new HashMap<>();
   }
 
   public void insert(String key, int val) {
-
+    assert key != null;
+    mapSum.put(key, val);
   }
 
   public int sum(String prefix) {
-    return 0;
+    int total = mapSum.keySet().stream().filter(s -> s.startsWith(prefix))
+        .mapToInt(s -> mapSum.get(s)).sum();
+    return total;
   }
-
 }
