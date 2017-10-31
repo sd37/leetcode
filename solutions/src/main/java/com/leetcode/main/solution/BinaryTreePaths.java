aab011e41066303620c1e1b76ecc8d053ffebaa3
spandan <spandan@prosperworks.com>
2017-10-30 20:58:59 -0700

    solution updated

--- solutions/src/main/java/com/leetcode/main/solution/BinaryTreePaths.java
+++ solutions/src/main/java/com/leetcode/main/solution/BinaryTreePaths.java
@@ -1,10 +1,38 @@
+// Status = AC
+// https://leetcode.com/problems/binary-tree-paths/description/
 package com.leetcode.main.solution;
 
 import com.leetcode.common.TreeNode;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTreePaths {
+
   public List<String> binaryTreePaths(TreeNode root) {
-    return null;
+    List<String> result = new ArrayList<>();
+
+    if (root == null) {
+      return result;
+    }
+
+    binaryTreePathHelper(root, result, "");
+    return result;
+  }
+
+  public void binaryTreePathHelper(TreeNode root, List<String> result, String curr) {
+    if (root.left == null && root.right == null) {
+      result.add(curr + root.val);
+      return;
+    }
+
+    curr = curr + root.val;
+
+    if (root.left != null) {
+      binaryTreePathHelper(root.left, result, curr + "->");
+    }
+
+    if (root.right != null) {
+      binaryTreePathHelper(root.right, result, curr + "->");
+    }
   }
 }
