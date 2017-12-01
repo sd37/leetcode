f02adf92a1f5853587af8a201d25c95fbb4444a9
spandan <spandan@prosperworks.com>
2017-11-30 20:47:45 -0800

    ReverseString added

--- solutions/src/main/java/com/leetcode/main/solution/ReverseString.java
+++ solutions/src/main/java/com/leetcode/main/solution/ReverseString.java
@@ -1,8 +1,25 @@
+// https://leetcode.com/problems/reverse-string/description/
+// Status = AC
+
 package com.leetcode.main.solution;
 
 public class ReverseString {
 
   public String reverseString(String s) {
-    return null;
+    char[] charS = s.toCharArray();
+
+    int f = 0;
+    int l = s.length() - 1;
+
+    while (f <= l) {
+      char tmp = charS[f];
+      charS[f] = charS[l];
+      charS[l] = tmp;
+
+      f++;
+      l--;
+    }
+
+    return new String(charS);
   }
 }
