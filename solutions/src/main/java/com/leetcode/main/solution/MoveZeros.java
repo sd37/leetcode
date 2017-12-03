b1eaa1b3ea6176231334dc113c574b00df9c98b4
spandan <spandan@prosperworks.com>
2017-12-03 11:51:49 -0800

    move zero added

--- solutions/src/main/java/com/leetcode/main/solution/MoveZeros.java
+++ solutions/src/main/java/com/leetcode/main/solution/MoveZeros.java
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/move-zeroes/description/
+// Status = AC
+
 package com.leetcode.main.solution;
 
 public class MoveZeros {
@@ -7,14 +10,14 @@ public class MoveZeros {
     }
 
     int insertPos = 0;
-
-    for (int x : nums) {
-      if (x != 0) {
+    for(int x : nums) {
+      if(x != 0) {
         nums[insertPos] = x;
         insertPos++;
       }
     }
 
+    // fill rest with zeros.
     for (int i = insertPos; i < nums.length; i++) {
       nums[i] = 0;
     }
