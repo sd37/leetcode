c6f74643ba1c2f376bf1370b4b342a97bbf7725c
spandan <spandan@prosperworks.com>
2017-12-05 01:45:28 -0800

    BalancedBinaryTreeAdded

---
+++ solutions/src/test/java/com/leetcode/test/solution/BalancedBinaryTree.java
@@ -0,0 +1,22 @@
+package com.leetcode.test.solution;
+
+import com.leetcode.common.TreeNode;
+
+public class BalancedBinaryTree {
+  private int height(TreeNode root) {
+    if(root == null) {
+      return 0;
+    }
+
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  public boolean isBalanced(TreeNode root) {
+    if(root == null) {
+      return true;
+    }
+
+    return (Math.abs(height(root.left) - height(root.right)) <= 1) &&
+        isBalanced(root.left) && isBalanced(root.right);
+  }
+}
