353ad748c5c7e6ec84e448a6e87746641fca8d24
spandan <spandan@prosperworks.com>
2017-12-05 01:19:21 -0800

    topK frequent elements

--- solutions/src/main/java/com/leetcode/main/solution/TopKFrequent.java
+++ solutions/src/main/java/com/leetcode/main/solution/TopKFrequent.java
@@ -6,33 +6,34 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.stream.Collectors;
-import javafx.util.Pair;
 
 public class TopKFrequent {
 
   public List<Integer> topKFrequent(int[] nums, int k) {
     Map<Integer, Integer> countMap = new HashMap<>();
 
-    for (int i = 0; i < nums.length; i++) {
-      if (!countMap.containsKey(nums[i])) {
-        countMap.put(nums[i], 0);
-      }
-      countMap.put(nums[i], countMap.get(nums[i]) + 1);
+    for(int x : nums) {
+      countMap.put(x, countMap.getOrDefault(x, 0) + 1);
     }
 
-    Queue<Pair<Integer,Integer>> pq = new PriorityQueue<>(Comparator.comparing(Pair::getValue));
+    PriorityQueue<Entry<Integer, Integer>> pq = new PriorityQueue<>(
+        new Comparator<Entry<Integer, Integer>>() {
+          @Override
+          public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+            return Integer.compare(o1.getValue(), o2.getValue());
+          }
+        });
 
-    for (Integer key : countMap.keySet()) {
-      pq.add(new Pair<>(key, countMap.get(key)));
-
-      if (pq.size() > k) {
+    for (Entry<Integer, Integer> kv : countMap.entrySet()) {
+      pq.add(kv);
+      if(pq.size() > k) {
         pq.poll();
       }
     }
 
-    return pq.stream().map(p -> p.getKey()).sorted().collect(Collectors.toList());
+    return pq.stream().mapToInt(x -> x.getKey()).boxed().collect(Collectors.toList());
   }
 }
