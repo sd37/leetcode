78b0ee2f717c94b888deb8c0071492c324d0c625
spandan <spandan@prosperworks.com>
2017-12-08 21:18:26 -0800

    zigzag traversal

---
+++ solutions/src/main/java/com/leetcode/main/solution/BinaryTreeZigZagLevelOrder.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+// Status = AC
+
+package com.leetcode.main.solution;
+
+import com.leetcode.common.TreeNode;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class BinaryTreeZigZagLevelOrder {
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    List<List<Integer>> lOrder = new ArrayList<>();
+
+    if (root == null) {
+      return lOrder;
+    }
+
+    Map<Integer, List<Integer>> levelMap = new HashMap<>();
+
+    Deque<Entry<TreeNode, Integer>> q = new ArrayDeque<>();
+
+    q.add(new SimpleEntry<TreeNode, Integer>(root, 0));
+
+    while (!q.isEmpty()) {
+      Entry<TreeNode, Integer> elm = q.poll();
+      TreeNode curr = elm.getKey();
+      int level = elm.getValue();
+
+      if (!levelMap.containsKey(level)) {
+        levelMap.put(level, new ArrayList<>());
+      }
+      levelMap.get(level).add(curr.val);
+
+      if (curr.left != null) {
+        q.add(new SimpleEntry<TreeNode, Integer>(curr.left, level + 1));
+      }
+
+      if (curr.right != null) {
+        q.add(new SimpleEntry<TreeNode, Integer>(curr.right, level + 1));
+      }
+    }
+
+    for (int i = 0; i < levelMap.size(); i++) {
+      List<Integer> tmpList = levelMap.get(i);
+      if(i % 2 != 0) {
+        Collections.reverse(tmpList);
+      }
+      lOrder.add(tmpList);
+    }
+
+    return lOrder;
+  }
+}
