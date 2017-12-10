ae33eb2276e90889715e7b3716fd0880414f797d
spandan <spandan@prosperworks.com>
2017-12-09 23:33:59 -0800

    maxSoFar

---
+++ solutions/src/main/java/com/leetcode/main/solution/ValidParens.java
@@ -0,0 +1,30 @@
+package com.leetcode.main.solution;
+
+import java.util.Stack;
+
+public class ValidParens {
+  public boolean isValid(String s) {
+    if(s == null || "".equals(s)) {
+      return true;
+    }
+
+    Stack<Character> st = new Stack<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+
+      if(!st.isEmpty() && isOpp(st.peek(), ch)) {
+        st.pop();
+      } else{
+        st.push(ch);
+      }
+
+    }
+
+    return st.isEmpty();
+  }
+
+  private boolean isOpp(char x, char y) {
+    return (x == '(' && y == ')' || x == '[' && y == ']' || x == '{' && y == '}');
+  }
+}
