d82d9f23beb38fab84796996adde57b3f2734362
spandan <spandan@prosperworks.com>
2017-12-10 22:05:54 -0800

    NumArray updated

---
+++ solutions/src/main/java/com/leetcode/main/solution/NumArray.java
@@ -0,0 +1,22 @@
+package com.leetcode.main.solution;
+
+public class NumArray {
+  class SegmentTree {
+  }
+
+
+  private int[] nums;
+
+  public NumArray(int[] nums) {
+    this.nums = nums;
+  }
+
+  public void update(int i, int val) {
+
+  }
+
+  public int sumRange(int i, int j) {
+
+    return 0;
+  }
+}
