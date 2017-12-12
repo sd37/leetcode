cd197e725f42f3f7eb03b162badedbef21dadadd
spandan <spandan@prosperworks.com>
2017-12-11 22:45:01 -0800

    ValidPerfectSquare added

--- solutions/src/main/java/com/leetcode/main/solution/ValidPerfectSquare.java
+++ solutions/src/main/java/com/leetcode/main/solution/ValidPerfectSquare.java
@@ -1,15 +1,24 @@
+// https://leetcode.com/problems/valid-perfect-square/discuss/
+// Status = AC
 package com.leetcode.main.solution;
 
 public class ValidPerfectSquare {
 
   public boolean isPerfectSquare(int num) {
-    int i;
-    for (i = 1; i * i < num; i++) {
+    long l = 1;
+    long r = num;
 
-    }
+    while (l <= r) {
+      long mid = l + (r - l) / 2;
+      long sq = mid * mid;
 
-    if (i * i == num) {
-      return true;
+      if (sq == num) {
+        return true;
+      } else if(sq < num) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
 
     return false;
