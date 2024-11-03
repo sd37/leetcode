7771117f48184453028935f972e15765e72a143f
Spandan Dutta <spdutta@microsoft.com>
2024-11-03 01:21:14 -0700

    changes

--- solutions/src/main/java/com/leetcode/main/solution/CourseSchedule.java
+++ solutions/src/main/java/com/leetcode/main/solution/CourseSchedule.java
@@ -8,57 +8,66 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-class Graph {
-    private int V;
-    private List<List<Integer>> adjList;
-
-    public Graph(int v) {
-        this.V = v;
-        this.adjList = new ArrayList<>();
-
-        for (int i = 0; i < v; i++) {
-            this.adjList.add(new ArrayList<>());
-        }
-    }
-
-    public void addEdge(int u, int v) {
-        adjList.get(u).add(v);
-    }
-
-    public boolean isCyclic() {
-        Set<Integer> seen = new HashSet<>(); // needed to track of disconnected components.
-        Set<Integer> recStack = new HashSet<>();
-
-        for (int i = 0; i < this.V; i++) {
-            if (!seen.contains(i) && isCyclicUtil(i, seen, recStack)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isCyclicUtil(int v, Set<Integer> seen, Set<Integer> recStack) {
-        seen.add(v);
-        recStack.add(v);
-
-        var neighbors = this.adjList.get(v);
-
-        for (var x : neighbors) {
-            if (!seen.contains(x) && isCyclicUtil(x, seen, recStack)) {
-                return true;
-            }
-            else if (recStack.contains(x)) {
-                return true;
-            }
-        }
-
-        recStack.remove(v);
-        return false;
-    }
-}
-
 public class CourseSchedule {
+    class Graph {
+        private int V;
+        private List<List<Integer>> adjList;
+
+        public Graph(int v) {
+            this.V = v;
+            this.adjList = new ArrayList<>();
+
+            for (int i = 0; i < v; i++) {
+                this.adjList.add(new ArrayList<>());
+            }
+        }
+
+        public void addEdge(int u, int v) {
+            adjList.get(u).add(v);
+        }
+
+        public boolean isCyclic() {
+            Set<Integer> seen = new HashSet<>(); // needed to track of disconnected components.
+            Set<Integer> recStack = new HashSet<>();
+
+            for (int i = 0; i < this.V; i++) {
+                if (seen.contains(i)) {
+                    continue;
+                }
+
+                if (isCyclicUtil(i, seen, recStack)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private boolean isCyclicUtil(int v, Set<Integer> seen, Set<Integer> recStack) {
+            seen.add(v);
+            recStack.add(v);
+
+            var neighbors = this.adjList.get(v);
+
+            for (var x : neighbors) {
+                if (seen.contains(x) && recStack.contains(x)) {
+                    return true;
+                }
+
+                if (seen.contains(x)) {
+                    continue;
+                }
+
+                if (isCyclicUtil(x, seen, recStack)) {
+                    return true;
+                }
+            }
+
+            recStack.remove(v);
+            return false;
+        }
+    }
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if (numCourses == 0) {
             return true;
--- solutions/src/main/java/com/leetcode/main/solution/CourseScheduleII.java
+++ solutions/src/main/java/com/leetcode/main/solution/CourseScheduleII.java
@@ -3,121 +3,123 @@
 
 package com.leetcode.main.solution;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-
+import java.util.*;
 
 public class CourseScheduleII {
+    class Graph {
+        private int V;
+        private List<List<Integer>> adjList;
 
-  class Graph {
+        public Graph(int V) {
+            this.V = V;
+            this.adjList = new ArrayList<>();
 
-    private int V;
-    private List<List<Integer>> adjList;
-
-    public Graph(int v) {
-      this.V = v;
-      this.adjList = new ArrayList<>();
-
-      for (int i = 0; i < v; i++) {
-        this.adjList.add(new ArrayList<>());
-      }
-    }
-
-    public void addEdge(int u, int v) {
-      adjList.get(u).add(v);
-    }
-
-    public boolean isCyclic() {
-      Set<Integer> seen = new HashSet<>();
-      Set<Integer> recStack = new HashSet<>();
-
-      for (int i = 0; i < V; i++) {
-        if (!seen.contains(i) && isCyclicUtil(i, seen, recStack)) {
-          return true;
+            for (int i = 0; i < this.V; i++) {
+                this.adjList.add(new ArrayList<>());
+            }
         }
-      }
 
-      return false;
-    }
-
-    private boolean isCyclicUtil(int v, Set<Integer> seen, Set<Integer> recStack) {
-      seen.add(v);
-      recStack.add(v);
-
-      List<Integer> neighbours = adjList.get(v);
-
-      for (int x : neighbours) {
-        if (!seen.contains(x) && isCyclicUtil(x, seen, recStack)) {
-          return true;
-        } else if (recStack.contains(x)) {
-          return true;
+        public void addEdge(int u, int v) {
+            this.adjList.get(u).add(v);
         }
-      }
 
-      recStack.remove(v);
-      return false;
-    }
+        public boolean isCyclic() {
+            Set<Integer> seen = new HashSet<>();
+            Set<Integer> recStack = new HashSet<>();
 
-    public List<Integer> topSort() {
-      Set<Integer> seen = new HashSet<>();
-      Stack<Integer> st = new Stack<>();
+            for (int i = 0; i < V; i++) {
+                if (seen.contains(i)) {
+                    continue;
+                }
 
-      List<Integer> order = new ArrayList<>();
+                if (isCyclicUtil(i, seen, recStack)) {
+                    return true;
+                }
+            }
 
-      for (int i = 0; i < V; i++) {
-        if (!seen.contains(i)) {
-          topSortUtil(i, seen, st);
+            return false;
         }
-      }
 
-      while (!st.empty()) {
-        order.add(st.pop());
-      }
+        public List<Integer> topSort() {
+            Set<Integer> seen = new HashSet<>();
+            Stack<Integer> st = new Stack<>();
 
-      return order;
-    }
+            for (int i = 0; i < this.V; i++) {
+                if (seen.contains(i)) {
+                    continue;
+                }
 
-    private void topSortUtil(int v, Set<Integer> seen, Stack<Integer> st) {
-      seen.add(v);
+                topSortUtil(i, seen, st);
+            }
 
-      List<Integer> neighbours = adjList.get(v);
-
-      for (int x : neighbours) {
-        if (!seen.contains(x)) {
-          topSortUtil(x, seen, st);
+            var result = new ArrayList<Integer>();
+            while(!st.empty()) {
+                var x = st.pop();
+                result.add(x);
+            }
+            return result;
         }
-      }
 
-      // this push to stack of v is key to topSort
-      st.push(v);
+        private void topSortUtil(int v, Set<Integer> seen, Stack<Integer> st) {
+            seen.add(v);
+            var neighbors = this.adjList.get(v);
+
+            for (var x : neighbors) {
+                if(seen.contains(x)) {
+                    continue;
+                }
+
+                topSortUtil(x, seen, st);
+            }
+
+            // push when v is completely explored.
+            st.push(v);
+        }
+
+        private boolean isCyclicUtil(int v, Set<Integer> seen, Set<Integer> recStack) {
+            seen.add(v);
+            recStack.add(v);
+
+            var neighbors = this.adjList.get(v);
+
+            for (var x : neighbors) {
+                if (seen.contains(x) && recStack.contains(x)) {
+                    return true;
+                }
+
+                if (seen.contains(x)) {
+                    continue;
+                }
+
+                if (isCyclicUtil(x, seen, recStack)) {
+                    return true;
+                }
+            }
+
+            recStack.remove(v);
+            return false;
+        }
     }
-  }
 
-  public int[] findOrder(int numCourses, int[][] prerequisites) {
-    if (numCourses == 0) {
-      return new int[]{};
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0) {
+            return new int[]{};
+        }
+
+        Graph g = new Graph(numCourses);
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            var u = prerequisites[i][0];
+            var v = prerequisites[i][1];
+
+            g.addEdge(v, u);
+        }
+
+        if (g.isCyclic()) {
+            return new int[]{};
+        }
+
+        var order = g.topSort();
+        return order.stream().mapToInt(x -> x).toArray();
     }
-
-    Graph g = new Graph(numCourses);
-
-    for (int i = 0; i < prerequisites.length; i++) {
-      g.addEdge(prerequisites[i][1], prerequisites[i][0]);
-    }
-
-    if (g.isCyclic()) {
-      return new int[]{};
-    }
-
-    List<Integer> order = g.topSort();
-
-    int[] orderArray = new int[order.size()];
-    for (int i = 0; i < order.size(); i++) {
-      orderArray[i] = order.get(i);
-    }
-
-    return orderArray;
-  }
 }
